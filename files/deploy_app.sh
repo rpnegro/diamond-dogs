f957a1b5a4c34a16e8bb1551e96f69d47c6defc8
rpnegro <rpnegro@gmail.com>
2022-02-09 11:52:16 -0500

    Modify frontend page

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -16,7 +16,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${placeholder}/${width}/${height}"></img></center>
   <center><h2>Diamond Dogs</h2></center>
-  Welcome to ${project}'s app.
+  Welcome to ${project}'s app. Nice page!
   <!-- END -->
 
   </div>
